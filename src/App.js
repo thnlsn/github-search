536a2d52f6c5046c0dec3679794a7742bc560f2e
Thomas Nelson <thnlsn@gmail.com>
2019-11-27 16:45:14 -0800

    Finished setting up React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment, Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const name = 'John Doe';
+    return (
+      <div className='App'>
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
