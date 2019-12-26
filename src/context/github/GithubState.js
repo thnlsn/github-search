661e5dfd9cfe0780334e267bec673e3f3bc6ca8f
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-12-25 23:34:54 -0800

    set up initial state in the state context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,1 +1,22 @@
-// All of our actions will go here, such as fetching data etc.
+// Initial state and all of our actions will go here, such as fetching data etc.
+
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+    SEARCH_USERS,
+    SET_LOADING,
+    CLEAR_USERS,
+    GET_USER,
+    GET_REPOS
+} from '../types';
+
+const GithubState = props => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false
+    };
+};
--- src/context/types.js
+++ src/context/types.js
@@ -1,1 +1,10 @@
-// types are variables on strings
+// types are variables of strings that can be called to change state within the reducer.
+
+// there are the types needed for this application
+export const SEARCH_USERS = 'SEARCH_USERS';
+export const GET_USERS = 'GET_USERS';
+export const CLEAR_USERS = 'CLEAR_USERS';
+export const GET_REPOS = 'GET_REPOS';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
