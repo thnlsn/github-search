d744f5bd87105e72258289817d73a40efc24b02b
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-01-04 13:37:11 -0800

    test

--- src/components/repos/Repos.js
+++ src/components/repos/Repos.js
@@ -3,15 +3,6 @@ import PropTypes from 'prop-types';
 import RepoItem from './RepoItem';
 
 const Repos = ({ repos, repoNum }) => {
-    console.log(
-        `▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓`
-    );
-    console.log(`TYPEOF REPONUM: ${typeof repoNum}`);
-    console.log(`REPONUM BEFORE SLICE: ${repoNum}`);
-    console.log(`REPOS BEFORE SLICE: ${repos}`);
-    // let reposToMap = repos.slice(0, repoNum);
-    console.log(`REPONUM AFTER SLICE: ${repoNum}`);
-    // console.log(`REPOS AFTER SLICE: ${reposToMap}`);
     return repos
         .slice(0, repoNum)
         .map(repo => <RepoItem repo={repo} key={repo.id} />);
